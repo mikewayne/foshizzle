d25d1ba2dc65a01decbeab2e72de3f2779f26fd9
mikewayne <mhoeft1@my.chemeketa.edu>
2015-04-20 17:42:48 -0800

    Update foshizzle.rb

--- lib/foshizzle.rb
+++ lib/foshizzle.rb
@@ -1,10 +1,15 @@
 class String
   define_method(:foshizzle) do
-    #if [0] == 's' || 'S'
-    #  false
-    #end
-    text = Array(self.length())
-    words = self.split("")
-    #self.is_lower?()
-  end
-end
+    newChars = []
+    originalChars = self.split("")
+    s_Char = ["s"]
+      originalChars.each() do |char|
+        if s_Char.include?(char)
+          newChars.push("z")
+        else
+          newChars.push(char)
+        end  
+      end
+      newChars.join()
+    end
+ end
